5bb1da4a50fb50b537c89f0f631df6f9e00621f4
joewachira <joewachira49@gmail.com>
2017-11-24 11:52:41 +0300

    add random generator file

--- comments.py
+++ comments.py
@@ -1,14 +1,21 @@
 __author__ = 'joe'
 
 import requests
+import click
+from .random import get_random_id
 
 
+@click.command()
+@click.option('--count', default=1, help='Number of greetings.')
+@click.option('--name', prompt='Your name',
+			  help='The person to greet.')
 def add_comment(self, post_id, title, body):
 	# This method will make an API call to add a comment to a given post
+	self.post_id = get_random_id()
 
 	data = {"id": post_id, "title": title, "body": body}
 	try:
-		resp = requests.post('http://34.207.10.230:3000/comments', json=data)
+		resp = requests.post('/comments', json=data)
 		if resp.status_code == 200:
 			# this means the post was made successfully
 			return {"content": resp.content,
---
+++ random.py
@@ -0,0 +1,9 @@
+__author__ = 'joe'
+import random
+
+# method generated random integer to be used as post ID
+
+
+def get_random_id():
+	random_id = random.randrange(1, 100000)
+	return random_id
