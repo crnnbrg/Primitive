6fd6dca48cebc12984904786686c155e13958073
EdwardMudaida <edwardmudaida@gmail.com>
2017-11-24 13:02:00 +0300

    COmbine view posts & create post

--- primefeed.py
+++ primefeed.py
@@ -2,6 +2,20 @@ import click
 import requests
 import sys
 
+
+@click.command()
+@click.option("--view_feed", is_flag=True, help="View posts.")
+
+def cli(view_feed):
+    if view_feed:
+        resp = requests.get('https://jsonplaceholder.typicode.com/posts')
+        if resp.status_code == 200:
+            for post in resp.json():
+                print("" * 20)
+                print("Title: " + post['title'])
+                print("" * 20)
+                print("Body " + post['body'])
+
 @click.command()
 @click.argument("title")
 @click.argument("body")
@@ -15,4 +29,3 @@ def cli(title, body):
     }
     response = requests.post("https://jsonplaceholder.typicode.com/posts", data=data)
     print(response.status_code)
-
