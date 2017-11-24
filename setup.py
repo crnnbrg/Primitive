23161e3651880d93a5fbbdc9e72320d991d89df4
EdwardMudaida <edwardmudaida@gmail.com>
2017-11-24 10:54:03 +0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="primefeed",
+    version="0.1",
+    py_modules=["primefeed"],
+    install_requires=[
+        "click",
+    ],
+    entry_points="""
+        [console_scripts],
+        primefeed=primefeed:cli
+    """,
+    )
