eba41447128cd6695811188b2f2afac1d777b118
joewachira <joewachira49@gmail.com>
2017-11-24 12:07:08 +0300

    modify comments file

--- comments.py
+++ comments.py
@@ -2,21 +2,25 @@ __author__ = 'joe'
 
 import requests
 import click
-from .random import get_random_id
+import random
 
 
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
 			  help='The person to greet.')
+def get_random_id():
+	random_id = random.randrange(1, 100000)
+	return random_id
+
 def add_comment(self, post_id, title, body):
 	# This method will make an API call to add a comment to a given post
 	self.post_id = get_random_id()
 
 	data = {"id": post_id, "title": title, "body": body}
 	try:
-		resp = requests.post('/comments', json=data)
-		if resp.status_code == 200:
+		resp = requests.post('https://jsonplaceholder.typicode.com/posts', json=data)
+		if resp.status_code == 201:
 			# this means the post was made successfully
 			return {"content": resp.content,
 					"message": "post added successfully"}
--- random.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'joe'
-import random
-
-# method generated random integer to be used as post ID
-
-
-def get_random_id():
-	random_id = random.randrange(1, 100000)
-	return random_id
